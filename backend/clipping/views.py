d590be6a1d7266b6257d0e5e9ad3709ee003bfb5
minhee <minheekim3@naver.com>
2022-02-14 18:00:14 +0900

    setting

--- backend/clipping/views.py
+++ backend/clipping/views.py
@@ -5,4 +5,9 @@ def base(request):
     '''
     general page
     '''
-    return render(request, 'clipping/clipping.html')
+    # db연결 필요
+    values = {
+        'groups': ['그룹1', '그룹2'],
+        'keywords': ['키워드1', '키워드2'],
+    }
+    return render(request, 'clipping/clipping.html', values)
