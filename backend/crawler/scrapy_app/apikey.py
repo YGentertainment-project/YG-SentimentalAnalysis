bb332494e962ba993178419c9688b4bd62467625
minhee <minheekim3@naver.com>
2022-02-17 18:03:57 +0900

    Revert "Revert "preview""
    This reverts commit 245df6639fec0974c8388109deca93ad73b4b68b.

---
+++ backend/crawler/scrapy_app/apikey.py
@@ -0,0 +1,39 @@
+#--------------------------------------
+# Youtube Data API Key (https://console.cloud.google.com/apis/credentials?hl=ko)
+YT_APIKEY='AIzaSyBkB398K_tVFWGOIYKJsSaGnMCraMFyckc'
+#--------------------------------------
+# Naver Search API ID & Secret
+NAVER_ID='aGJg_4Rokn739onM1tbW'
+NAVER_SECRET='yDpmbubfYn'
+#--------------------------------------
+# Server SSH Tunnel
+SSH_ENABLE=False
+SSH_MARIA_ENABLE=False
+SSH_HOST='115.145.212.145'
+SSH_PORT=3322
+SSH_USER='skku-yg'
+SSH_PSWD='skkuyg'
+#--------------------------------------
+# MariaDB
+SQL_HOST='150.230.34.147'
+SQL_PORT=3306
+SQL_DB='ygenter2'
+SQL_USER='ygenter'
+SQL_PW='ygenter'
+#--------------------------------------
+# MongoDB 
+MONGO_ADDR='150.230.34.147'
+MONGO_PORT=27017
+MONGO_DB='crawling_tuto'
+MONGO_USER='ygenter'
+MONGO_PSWD='ygenter'
+#--------------------------------------
+# ChromeDriver
+CHROMEDRIVER_PATH='./YG_Crawler/chromedriver'
+#--------------------------------------
+# CrowdTangle
+CROWDTANGLE_EMAIL='jaewon@ygmail.net'
+CROWDTANGLE_PASSWORD='Ygfamily1234@'
+CROWDTANGLE_IG_LINK='https://apps.crowdtangle.com/ygskkuig'
+CROWDTANGLE_FB_LINK='https://apps.crowdtangle.com/ygskkufb'
+#--------------------------------------
--- backend/report/urls.py
+++ backend/report/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.base, name="report"),
     path("preview/", views.preview, name="preview"),
     path('load_data/', views.load_data, name='load_data'),
+    path('load_preview/', views.load_preview, name='load_preview'),
 ]
