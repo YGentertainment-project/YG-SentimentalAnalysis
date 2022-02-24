b40dd100ffa561f49b888427a2a493f53e7802f3
tsfo1489 <tsfo1489@gmail.com>
2022-02-24 20:35:27 +0900

    [Fix] Fix production setting

--- backend/yg/production_settings.py
+++ backend/yg/production_settings.py
@@ -2,25 +2,25 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "ygenter2",
+        'NAME': "ygenter",
         'USER': "ygenter",
         'PASSWORD': "ygenter",
-        'HOST': "yg-mariadb",
+        'HOST': "yg-as-mariadb",
         'PORT': 3306,
     },
-    # "mongo": {
-    #     "ENGINE": "djongo",
-    #     # "ENFORCE_SCHEMA": True,
-    #     "NAME": "yg-mongodb",
-    #     "CLIENT": {
-    #         "host": "ygenter",
-    #         "port": 27017,
-    #         "username": "ygenter",
-    #         "password": "ygenter",
-    #         "authSource": "admin",
-    #         "authMechanism": "SCRAM-SHA-1"
-    #     }
-    # }
+    "mongo": {
+        "ENGINE": "djongo",
+        # "ENFORCE_SCHEMA": True,
+        "NAME": "yg-mongodb",
+        "CLIENT": {
+            "host": "yg-as-mongodb",
+            "port": 27017,
+            "username": "ygenter",
+            "password": "ygenter",
+            "authSource": "admin",
+            "authMechanism": "SCRAM-SHA-1"
+        }
+    }
 }
 
 DEBUG = False
