98c74564c26eb85b1a050bfeb44c5abcd3b3146a
tsfo1489 <tsfo1489@gmail.com>
2022-03-25 09:53:15 +0900

    [Feat] Upload NER

---
+++ NLP/user-dic/mecab_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt update
+apt-get -y install openjdk-8-jdk python-dev python3-dev
+pip3 install konlpy jpype1-py3
+
+bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)
+
+cd /tmp/mecab-ko-dic-2.1.1-20180720
+ldconfig
+ldconfig -p | grep /usr/local/lib
+
+cd /tmp/mecab-0.996-ko-0.9.2
+./configure
+make
+make check
+make install
+
+cd /tmp/mecab-ko-dic-2.1.1-20180720
+./autogen.sh
+./configure
+make
+make install
+
+sh apply.sh
