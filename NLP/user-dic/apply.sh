ed83b79630b06be84e168ce2a5c2d13abbf767d8
Jaewon Kwak <tsfo1489@gmail.com>
2022-03-30 11:59:20 +0900

    [Fix] Fix Path in Apply.sh

--- NLP/user-dic/apply.sh
+++ NLP/user-dic/apply.sh
@@ -1,4 +1,4 @@
-cp ./workspace/user-dic/*.csv /tmp/mecab-ko-dic-2.1.1-20180720/user-dic/
+cp ./user-dic/*.csv /tmp/mecab-ko-dic-2.1.1-20180720/user-dic/
 cd /tmp/mecab-ko-dic-2.1.1-20180720/tools
 ./add-userdic.sh
 cd /tmp/mecab-ko-dic-2.1.1-20180720
